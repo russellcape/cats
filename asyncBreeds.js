8f8a5e44fe5f6e2f4100dcd7fa4345f6a6a72968
Russell Cape <russellcape98@gmail.com>
2019-10-24 22:58:19 +0000

    Cat assertion

---
+++ asyncBreeds.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  //console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    //console.log('Callback: I have the data!')
+    if (!error) {
+      callback(data);
+    }
+  })
+};
+
+module.exports = breedDetailsFromFile;
